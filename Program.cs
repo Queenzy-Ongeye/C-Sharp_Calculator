3aa6663f804394cb00b519f37a39d970ac672f4e
Queen-02 <queenzyblack176@gmail.com>
2022-08-11 14:02:20 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.Write("Enter a number: ");
+        double num_one = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Enter Operator: ");
+        string op = Console.ReadLine();
+
+        Console.Write("Enter a number: ");
+        double num_two = Convert.ToDouble(Console.ReadLine());
+
+        if (op == "+")
+        {
+            Console.WriteLine(num_one + num_two);
+        }
+        else if (op == "-")
+        {
+            Console.WriteLine(num_one - num_two);
+        }
+        else if (op == "/")
+        {
+            Console.WriteLine(num_one / num_two);
+        }
+        else if (op == "*")
+        {
+            Console.WriteLine(num_one * num_two);
+        }
+        else
+        {
+            Console.WriteLine("Invalid operator");
+        }
+    }
+}
